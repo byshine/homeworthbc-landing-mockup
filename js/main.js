efb23ef54208580614edfed0fd27fce12e875cfe
aaron <aaron@countable.ca>
2018-04-11 11:52:23 -0700

    Improvments

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([49.2827, -123.1207], 15);
+$('.leaflet-control-attribution').hide();
+
+var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
