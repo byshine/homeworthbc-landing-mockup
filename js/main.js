63234199f2f86b9a95bccfd7e3d75bfa073fcf6c
aaron <aaron@countable.ca>
2018-04-12 13:42:58 -0700

    gooooogle maps

--- js/main.js
+++ js/main.js
@@ -1,10 +1,175 @@
-var map = L.map('map').setView([49.2827, -123.1207], 15);
-$('.leaflet-control-attribution').hide();
+function initMap() {
+	var uluru = {lat: 49.246292, lng: -123.116226};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 13,
+	  center: uluru,
+	  styles: [
+		{
+		  "elementType": "geometry",
+		  "stylers": [
+			{
+			  "color": "#f5f5f5"
+			}
+		  ]
+		},
+		{
+		  "elementType": "labels.icon",
+		  "stylers": [
+			{
+			  "visibility": "off"
+			}
+		  ]
+		},
+		{
+		  "elementType": "labels.text.fill",
+		  "stylers": [
+			{
+			  "color": "#616161"
+			}
+		  ]
+		},
+		{
+		  "elementType": "labels.text.stroke",
+		  "stylers": [
+			{
+			  "color": "#f5f5f5"
+			}
+		  ]
+		},
+		{
+		  "featureType": "administrative.land_parcel",
+		  "elementType": "labels.text.fill",
+		  "stylers": [
+			{
+			  "color": "#bdbdbd"
+			}
+		  ]
+		},
+		{
+		  "featureType": "poi",
+		  "elementType": "geometry",
+		  "stylers": [
+			{
+			  "color": "#eeeeee"
+			}
+		  ]
+		},
+		{
+		  "featureType": "poi",
+		  "elementType": "labels.text.fill",
+		  "stylers": [
+			{
+			  "color": "#757575"
+			}
+		  ]
+		},
+		{
+		  "featureType": "poi.park",
+		  "elementType": "geometry",
+		  "stylers": [
+			{
+			  "color": "#e5e5e5"
+			}
+		  ]
+		},
+		{
+		  "featureType": "poi.park",
+		  "elementType": "labels.text.fill",
+		  "stylers": [
+			{
+			  "color": "#9e9e9e"
+			}
+		  ]
+		},
+		{
+		  "featureType": "road",
+		  "elementType": "geometry",
+		  "stylers": [
+			{
+			  "color": "#ffffff"
+			}
+		  ]
+		},
+		{
+		  "featureType": "road.arterial",
+		  "elementType": "labels.text.fill",
+		  "stylers": [
+			{
+			  "color": "#757575"
+			}
+		  ]
+		},
+		{
+		  "featureType": "road.highway",
+		  "elementType": "geometry",
+		  "stylers": [
+			{
+			  "color": "#dadada"
+			}
+		  ]
+		},
+		{
+		  "featureType": "road.highway",
+		  "elementType": "labels.text.fill",
+		  "stylers": [
+			{
+			  "color": "#616161"
+			}
+		  ]
+		},
+		{
+		  "featureType": "road.local",
+		  "elementType": "labels.text.fill",
+		  "stylers": [
+			{
+			  "color": "#9e9e9e"
+			}
+		  ]
+		},
+		{
+		  "featureType": "transit.line",
+		  "elementType": "geometry",
+		  "stylers": [
+			{
+			  "color": "#e5e5e5"
+			}
+		  ]
+		},
+		{
+		  "featureType": "transit.station",
+		  "elementType": "geometry",
+		  "stylers": [
+			{
+			  "color": "#eeeeee"
+			}
+		  ]
+		},
+		{
+		  "featureType": "water",
+		  "elementType": "geometry",
+		  "stylers": [
+			{
+			  "color": "#c9c9c9"
+			}
+		  ]
+		},
+		{
+		  "featureType": "water",
+		  "elementType": "labels.text.fill",
+		  "stylers": [
+			{
+			  "color": "#9e9e9e"
+			}
+		  ]
+		}
+	  ]
 
-var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
-}).addTo(map);
+	});
+	var marker = new google.maps.Marker({
+	  position: uluru,
+	  map: map
+	});
+
+  }
+
+  
